1b51852788eb16b1bf00c6a33e85689cb5d9e245
irensmile <curkan.ira@gmail.com>
2024-02-21 11:11:00 -0500

    loader size change

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -1,15 +1,15 @@
 import { Audio } from "react-loader-spinner";
 
 export const Loader = () => (
-    <Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="blue"
-        ariaLabel="loading..."
-        wrapperStyle
-        wrapperClass
-    />
+  <Audio
+    height="40"
+    width="40"
+    radius="9"
+    color="blue"
+    ariaLabel="loading..."
+    wrapperStyle
+    wrapperClass
+  />
 );
 
 export default Loader;
